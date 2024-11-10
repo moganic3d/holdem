f8bd5728b128245dc4a7e7ba27d440156caafcc2
moganic3d <ggg@hotmail.co.kr>
2024-11-10 15:34:35 +0900

    Update poker_calculator.py

--- src/poker_calculator.py
+++ src/poker_calculator.py
@@ -7,7 +7,6 @@ class PokerCalculator:
         self.suits = 'HDCS'
         self.deck = [r + s for r in self.ranks for s in self.suits]
         
-        # 카드 이름 매핑
         self.rank_names = {
             'T': '10', 'J': 'Jack', 'Q': 'Queen', 'K': 'King', 'A': 'Ace'
         }
@@ -17,12 +16,48 @@ class PokerCalculator:
         }
 
     def get_card_name(self, card):
+        if card == "없음":
+            return "선택 안함"
         rank = self.rank_names.get(card[0], card[0])
         suit = self.suit_names[card[1]]
         return f"{rank} of {suit}"
 
-    # evaluate_hand 메서드는 이전 코드와 동일
-    # calculate_odds 메서드는 약간 수정하여 결과를 반환하도록 변경
+    def evaluate_hand(self, cards):
+        # 카드의 숫자와 무늬 분리
+        ranks = [card[0] for card in cards]
+        suits = [card[1] for card in cards]
+        
+        # 페어, 트리플, 포카드 확인
+        rank_counts = Counter(ranks)
+        
+        # 플러시 확인
+        is_flush = len(set(suits)) == 1
+        
+        # 스트레이트 확인
+        rank_values = [self.ranks.index(r) for r in ranks]
+        rank_values.sort()
+        is_straight = (len(set(rank_values)) == 5 and
+                      max(rank_values) - min(rank_values) == 4)
+        
+        # 핸드 랭크 결정
+        if is_straight and is_flush:
+            return 8, "스트레이트 플러시"
+        elif 4 in rank_counts.values():
+            return 7, "포카드"
+        elif sorted(rank_counts.values()) == [2,3]:
+            return 6, "풀하우스"
+        elif is_flush:
+            return 5, "플러시"
+        elif is_straight:
+            return 4, "스트레이트"
+        elif 3 in rank_counts.values():
+            return 3, "트리플"
+        elif list(rank_counts.values()).count(2) == 2:
+            return 2, "투페어"
+        elif 2 in rank_counts.values():
+            return 1, "원페어"
+        else:
+            return 0, "하이카드"
 
     def calculate_odds(self, hole_cards, community_cards=[]):
         remaining_cards = [card for card in self.deck 
@@ -44,8 +79,4 @@ class PokerCalculator:
                                      key=lambda x: (-x[1], x[0])):
             probability = (count / total_hands) * 100
             results.append({
-                "hand": hand_type,
-                "probability": probability
-            })
-        
-        return results 
+                "hand":
